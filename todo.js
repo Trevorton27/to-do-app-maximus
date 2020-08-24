a3f3439e2df81faf52a97eebd32fe5b375a94645
Trevor Mearns <Trevor Mearns>
2020-08-24 15:55:11 +0900

    code review based refactor finished. Thank you, Andy!

--- todo.js
+++ todo.js
@@ -1,72 +1,86 @@
 const todoItems = JSON.parse(localStorage.getItem('todoItems')) || [];
 
-//window.addEventListener("load", pullFromLocal);
-document.getElementById("js-form").addEventListener('submit', addTodo);
+document.getElementById('form').addEventListener('submit', addTodo);
+
+const submitButton = document.getElementById('submitButton');
+submitButton.addEventListener('click', addTodo);
 
 function addTodo(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    let text = document.getElementById('js-todo-input').value;
+  let text = document.getElementById('todo-input').value;
 
-    if(text.length > 0) {
-        const task = {
-            text,
-            checked: false,
-            id: todoItems.length > 0 ? todoItems[todoItems.length - 1].id + 1 : 1
-        };
+  if (text.length > 0) {
+    const task = {
+      text,
+      checked: false,
+      id: todoItems.length > 0 ? todoItems[todoItems.length - 1].id + 1 : 1,
+    };
 
-        todoItems.push(task);
-        localStorage.setItem('todoItems', JSON.stringify(todoItems));
+    todoItems.push(task);
+    saveTodoItems();
+  }
 
-        console.log('The task is: ', task);
-        console.log('todoItems: ', todoItems);
-    }
-
-    document.getElementById("js-form").reset();
-    renderTask();
-
-};
-
+  document.getElementById('form').reset();
+  renderTask();
+}
 
 function renderTask() {
-    //pullFromLocal();
-    const list = document.getElementById("js-todo-list");
+  const list = document.getElementById('todo-list');
 
-    list.innerHTML = '';
+  list.innerHTML = '';
 
-    for(let i =0; i < todoItems.length; i++){
-        let text = todoItems[i].text;
-        list.innerHTML += `
-        <li class="todo-item"    
-            onclick="toggleTask(${todoItems[i].id})"
-            id="${todoItems[i].id}">
-            ${todoItems[i].checked ? text.strike() : text}
-        <span class="delete-todo js-delete-todo"
-             onclick="deleteTask(${todoItems[i].id})">
-             <svg><use href="#delete-icon"></use></svg>
-         </span>
-        </li>`
+  for (let i = 0; i < todoItems.length; i++) {
+    let text = todoItems[i].text;
+
+    const li = document.createElement('LI');
+    li.className = 'todo-item';
+    li.addEventListener('click', function (e) {
+      toggleTask(todoItems[i].id);
+    });
+    li.id = todoItems[i].id;
+    li.textContent = text;
+    if (todoItems[i].checked === true) {
+      li.style.textDecoration = 'line-through';
     }
-     
-};
+    list.appendChild(li);
+
+    const span = document.createElement('SPAN');
+    span.className = 'delete-todo';
+
+    li.appendChild(span);
+    const deleteButton = document.createElement('BUTTON');
+    deleteButton.className = 'deleteButton';
+    deleteButton.textContent = 'X';
+    span.appendChild(deleteButton);
+
+    deleteButton.addEventListener('click', () => {
+      deleteTask(todoItems[i].id);
+    });
+  }
+}
+
+function saveTodoItems() {
+  localStorage.setItem('todoItems', JSON.stringify(todoItems));
+}
 
 function deleteTask(id) {
-    for(let i = 0; i < todoItems.length; i++) {
-        if(todoItems[i].id == id) {
-            todoItems.splice(i, 1);
-        }
+  for (let i = 0; i < todoItems.length; i++) {
+    if (todoItems[i].id == id) {
+      todoItems.splice(i, 1);
     }
-    localStorage.setItem('todoItems', JSON.stringify(todoItems));
-    renderTask();
-};
+  }
+  saveTodoItems();
+  renderTask();
+}
 
 function toggleTask(id) {
-    for(let i = 0; i < todoItems.length; i++){
-        if(todoItems[i].id == id) {
-            todoItems[i].checked = !todoItems[i].checked;
-        } 
+  for (let i = 0; i < todoItems.length; i++) {
+    if (todoItems[i].id == id) {
+      todoItems[i].checked = !todoItems[i].checked;
     }
-    renderTask();
-};
+  }
+  renderTask();
+}
 
 renderTask();
